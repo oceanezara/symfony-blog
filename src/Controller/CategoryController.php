84f5a196fb280d1956f12f53dc595f8873b67f8c
Océane Raubitschek <oceane@MacBook-Pro-de-Oceane.local>
2021-12-20 18:04:18 +0100

    add controllers

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'category')]
-    public function index(): Response
+    #[Route('/category/{name}', methods:['GET'], requirements:['name' => '[a-z]+'], name: 'category')]
+    public function index(string $name, CategoryRepository $categoryRepository): Response
     {
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'name' => $name,
         ]);
     }
 }
--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -19,6 +19,22 @@ class PostFixtures extends Fixture implements DependentFixtureInterface
             ->setCategory($this->getReference(CategoryFixtures::HORREUR));
         $manager->persist($post);
 
+        $post1 = new Post();
+        $post1->setName('Covid')
+            ->setCategory($this->getReference(CategoryFixtures::HORREUR));
+        $manager->persist($post1);
+
+        $post2 = new Post();
+        $post2->setName('Élections')
+            ->setCategory($this->getReference(CategoryFixtures::HORREUR));
+        $manager->persist($post2);
+
+        for ($i=1; $i <= 30; $i++) {
+            $post3 = new Post();
+            $post3->setName('Nom de catégorie ' .$i)
+            ->setCategory($this->getReference(CategoryFixtures::HORREUR));
+            $manager->persist($post3);
+        }
 
         $manager->flush();
     }
